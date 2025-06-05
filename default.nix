8ca392ed67c2727752067eee15f03f689d2fa075
Victor Hang <vhvictorhang@gmail.com>
2025-06-06 00:06:12 +0200

    fix 🐛: arc pvc
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ let
   entrypoint = pkgs.writeShellScriptBin "entrypoint.sh" ''
     #!/bin/sh
     sudo nix-daemon &
-    chown 1001:1001 -R /home/runner/.github-runner
+    sudo chown 1001:1001 -R /home/runner/.github-runner
     export ORGANIZATION=$ORGANIZATION
     export ACCESS_TOKEN=$ACCESS_TOKEN
     export REG_TOKEN=$(curl -sX POST -H "Authorization: token $ACCESS_TOKEN" https://api.github.com/orgs/$ORGANIZATION/actions/runners/registration-token | jq .token --raw-output)
